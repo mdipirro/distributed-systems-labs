5a36e1e6898eda77c4af7090f3d0a8acd14510dd
Matteo Di Pirro <matteodipirro@gmail.com>
2017-10-10 12:08:39 +0200

    Add first assignment draft

---
+++ AssignementRMI1/src/rental/CarRentalCompanyI.java
@@ -0,0 +1,15 @@
+package rental;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public interface CarRentalCompanyI extends Remote {
+	Set<CarType> getAvailableCarTypes(Date start, Date end) throws RemoteException;
+	List<String> getRegions() throws RemoteException;
+	Quote createQuote(ReservationConstraints constraints, String client)
+			throws ReservationException, RemoteException;
+	List<Reservation> getReservationsByRenter(String renter) throws RemoteException;
+}
