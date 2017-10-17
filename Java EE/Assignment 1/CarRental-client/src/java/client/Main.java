3655bbd1a134b062ddf513fbdd833d7661fa4bd2
Matteo Di Pirro <matteodipirro@gmail.com>
2017-10-17 12:28:19 +0200

    Add implementations of some AbstractTestAgency methods

--- Java EE/Assignment 1/CarRental-client/src/java/client/Main.java
+++ Java EE/Assignment 1/CarRental-client/src/java/client/Main.java
@@ -2,23 +2,60 @@ package client;
 
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ejb.EJB;
-import rental.Quote;
+import javax.naming.InitialContext;
+import rental.Reservation;
 import rental.ReservationConstraints;
-import rental.ReservationException;
 import session.CarRentalSessionRemote;
 
-public class Main {
-    
-    @EJB
-    static CarRentalSessionRemote session;
+public class Main extends AbstractTestAgency {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("found rental companies: "+session.getAllRentalCompanies());
-        
+    public static void main(String[] args) {}
+
+    public Main(String scriptFile) {
+        super(scriptFile);
+    }
+
+    @Override
+    protected CarRentalSessionRemote getNewReservationSession(String name) throws Exception {
+        InitialContext context = new InitialContext();
+        return (CarRentalSessionRemote) context.lookup(CarRentalSessionRemote.class.getName());
+    }
+
+    @Override
+    protected Object getNewManagerSession(String name, String carRentalName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void checkForAvailableCarTypes(Object session, Date start, Date end) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void addQuoteToSession(Object session, String name, Date start, Date end, String carType, String region) throws Exception {
+        if (session instanceof CarRentalSessionRemote) {
+            CarRentalSessionRemote bean = (CarRentalSessionRemote)session;
+            bean.createQuote(
+                    new ReservationConstraints(start, end, carType, region),
+                    name
+            );
+        }
+        throw new ClassCastException("Wrong session object provided");
+    }
+
+    @Override
+    protected List<Reservation> confirmQuotes(Object session, String name) throws Exception {
+        if (session instanceof CarRentalSessionRemote) {
+            CarRentalSessionRemote bean = (CarRentalSessionRemote)session;
+            return bean.confirmQuotes();
+        }
+        throw new ClassCastException("Wrong session object provided");
+    }
+
+    @Override
+    protected int getNumberOfReservationsForCarType(Object ms, String carRentalName, String carType) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
