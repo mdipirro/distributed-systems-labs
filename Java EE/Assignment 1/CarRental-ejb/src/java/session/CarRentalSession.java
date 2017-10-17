1dbe7bcd6b1214b2f991f33dc73cbf0bb8c06401
Matteo Di Pirro <matteodipirro@gmail.com>
2017-10-17 11:48:29 +0200

    Add methods for exercise 4.1

--- Java EE/Assignment 1/CarRental-ejb/src/java/session/CarRentalSession.java
+++ Java EE/Assignment 1/CarRental-ejb/src/java/session/CarRentalSession.java
@@ -1,18 +1,67 @@
 package session;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
+import rental.CarRentalCompany;
+import rental.Quote;
 import rental.RentalStore;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import rental.ReservationException;
 
 @Stateful
 public class CarRentalSession implements CarRentalSessionRemote {
+    
+    private List<Quote> quotes;
+    
+    public CarRentalSession() {
+        quotes = new ArrayList<Quote>();
+    }
 
     @Override
     public Set<String> getAllRentalCompanies() {
         return new HashSet<String>(RentalStore.getRentals().keySet());
     }
 
-    
-    
+    @Override
+    public Quote createQuote(ReservationConstraints constraints, String guest)
+    throws ReservationException {
+        boolean companyFound = false;
+        Iterator<CarRentalCompany> iterator = RentalStore.getRentals().values().iterator();
+        CarRentalCompany company = null;
+        // Look for a company having the requested region 
+        while (iterator.hasNext() && !companyFound) {
+            company = iterator.next();
+            companyFound = (company.hasRegion(constraints.getRegion()));
+        }
+        if (company != null) { // A company has been found
+            Quote quote = company.createQuote(constraints, guest);
+            quotes.add(quote);
+            return quote;
+        }
+        // No companies with the requested region, throw an exception
+        throw new ReservationException("No available company with the "
+                + "requested region <" + constraints.getRegion() + ">");
+    }
+
+    @Override
+    public Reservation confirmQuote(Quote quote) throws ReservationException {
+        try {
+            return RentalStore.getRental(quote.getRentalCompany()).confirmQuote(quote);
+        } catch (ReservationException exc) {
+            quotes.remove(quote);
+            throw exc;
+        }
+    }
+
+    @Override
+    public List<Quote> getCurrentQuotes() {
+        return quotes;
+    }
 }
--- Java EE/Assignment 1/CarRental-lib/src/session/CarRentalSessionRemote.java
+++ Java EE/Assignment 1/CarRental-lib/src/session/CarRentalSessionRemote.java
@@ -1,11 +1,20 @@
 package session;
 
+import java.util.List;
 import java.util.Set;
 import javax.ejb.Remote;
+import rental.Quote;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import rental.ReservationException;
 
 @Remote
 public interface CarRentalSessionRemote {
 
     Set<String> getAllRentalCompanies();
+    Quote createQuote(ReservationConstraints constraints, String guest)
+            throws ReservationException;
+    List<Quote> getCurrentQuotes();
+    Reservation confirmQuote(Quote quote) throws ReservationException;
     
 }
