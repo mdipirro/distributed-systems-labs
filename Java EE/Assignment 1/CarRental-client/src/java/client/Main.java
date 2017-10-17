ac635e01926b1ddc5042db1b1caa3a011d83d4d0
Matteo Di Pirro <matteodipirro@gmail.com>
2017-10-17 11:48:54 +0200

    Some tests with Main

--- Java EE/Assignment 1/CarRental-client/src/java/client/Main.java
+++ Java EE/Assignment 1/CarRental-client/src/java/client/Main.java
@@ -1,6 +1,13 @@
 package client;
 
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
+import rental.Quote;
+import rental.ReservationConstraints;
+import rental.ReservationException;
 import session.CarRentalSessionRemote;
 
 public class Main {
@@ -12,5 +19,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("found rental companies: "+session.getAllRentalCompanies());
+        
     }
 }
