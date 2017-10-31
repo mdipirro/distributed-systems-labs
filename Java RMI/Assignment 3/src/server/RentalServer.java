e54c8ce6f4771978720dfc783e14078c8fcf46ec
Matteo Di Pirro <matteodipirro@gmail.com>
2017-10-31 12:30:13 +0100

    CHANGE RentalServer with Jakub's additions

--- Java RMI/Assignment 3/src/server/RentalServer.java
+++ Java RMI/Assignment 3/src/server/RentalServer.java
@@ -1,8 +1,13 @@
 package server;
 
+import configuration.AgencyConfigRMI;
 import rental.*;
-import rentalstore.NamingService;
-import rentalstore.NamingServiceRemote;
+import namingservice.NamingService;
+import namingservice.NamingServiceRemote;
+import session.AgencySessionManager;
+import session.AgencySessionManagerRemote;
+import session.ManagerSession;
+import session.ManagerSessionRemote;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
@@ -12,30 +17,38 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class RentalServer {
 
-    public static final String MANAGER_SESSION_NAME = "MANAGER_SESSION";
-	public static final String RENTAL_SESSION_MANAGER_NAME = "RENTAL_SESSION_MANAGER";
-    public static final String NAMING_SERVICE_NAME = "NAMING_SERVICE";
+    Registry registry;
 
-	public static void main(String[] args) throws ReservationException,
-			NumberFormatException, IOException {
-		ManagerSession mgr = new ManagerSession();
-		RentalSessionManager rtlManager = new RentalSessionManager();
-        NamingService namingService = new NamingService();
+    public static void main(String[] args) throws ReservationException,
+            NumberFormatException, IOException {
+        System.setSecurityManager(null);
+        RentalServer rentalServer = new RentalServer();
+        rentalServer.setupDependencies();
 
-		System.setSecurityManager(null);
-		try {
-			ManagerSessionRemote msStub = (ManagerSessionRemote)
-                    UnicastRemoteObject.exportObject(mgr, 0);
-            RentalSessionManagerRemote rmgrStub = (RentalSessionManagerRemote)
-                    UnicastRemoteObject.exportObject(rtlManager, 0);
-            NamingServiceRemote namingStub = (NamingServiceRemote)
-                    UnicastRemoteObject.exportObject(namingService, 0);
-			Registry registry = LocateRegistry.getRegistry();
-			registry.rebind(MANAGER_SESSION_NAME, msStub);
-            registry.rebind(RENTAL_SESSION_MANAGER_NAME, rmgrStub);
-            registry.rebind(NAMING_SERVICE_NAME, namingService);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
+        System.out.println("Rental agency started SUCCESSFULLY");
+    }
+
+    private void setupDependencies(){
+        try {
+            registry = LocateRegistry.getRegistry();
+
+            NamingService ns = new NamingService();
+            NamingServiceRemote namingServiceStub = (NamingServiceRemote)
+                    UnicastRemoteObject.exportObject(ns, 0);
+
+            AgencySessionManager asm = new AgencySessionManager(namingServiceStub);
+            AgencySessionManagerRemote asmStub = (AgencySessionManagerRemote)
+                    UnicastRemoteObject.exportObject(asm, 0);
+
+            ManagerSession ms = new ManagerSession(ns);
+            ManagerSessionRemote msStub = (ManagerSessionRemote)
+                    UnicastRemoteObject.exportObject(ms, 0);
+
+            registry.rebind(AgencyConfigRMI.NAMING_SERVICE_NAME, namingServiceStub);
+            registry.rebind(AgencyConfigRMI.AGENCY_SESSION_MANAGER_NAME, asmStub);
+            registry.rebind(AgencyConfigRMI.MANAGER_SESSION, msStub);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
