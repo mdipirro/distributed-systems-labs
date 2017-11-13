e47fa98384d0e744e34d7df7d7ce0b2995ed0de8
Matteo Di Pirro <matteodipirro@gmail.com>
2017-11-13 18:41:29 +0100

    RENAME Directories

---
+++ JavaEE/Assignment2-3/assignment2/CarRental-lib/src/session/ManagerSessionRemote.java
@@ -0,0 +1,21 @@
+package session;
+
+import java.util.List;
+import java.util.Set;
+import javax.ejb.Remote;
+import rental.CarType;
+
+@Remote
+public interface ManagerSessionRemote {
+    
+    Set<CarType> getCarTypes(String company);
+    Set<Integer> getCarIds(String company,String type);
+    int getNumberOfReservations(String company, String type, int carId);
+    int getNumberOfReservations(String company, String type);  
+    void loadRental(String datafile);
+    void addRentalCompany(String name, List<String> regions);
+    void addCar(String companyName, String carType);
+    void addCarType(String companyName, CarType carType);
+    public CarType getMostPopularCarType(String carRentalCompanyName, int year);
+    public Set<String> getBestClients();
+}
