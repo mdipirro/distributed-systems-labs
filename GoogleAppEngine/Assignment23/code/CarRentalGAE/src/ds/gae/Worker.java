240e4c8a881d2639143b062c5ee825f0a84bd08e
Matteo Di Pirro <matteodipirro@gmail.com>
2017-11-28 23:48:22 +0100

    ADD confirmQuotes implementation

--- GoogleAppEngine/Assignment23/code/CarRentalGAE/src/ds/gae/Worker.java
+++ GoogleAppEngine/Assignment23/code/CarRentalGAE/src/ds/gae/Worker.java
@@ -2,6 +2,7 @@ package ds.gae;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,8 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ds.gae.entities.CarRentalCompany;
 import ds.gae.entities.Quote;
 import ds.gae.entities.QuotesStatus;
+import ds.gae.entities.Reservation;
 
 public class Worker extends HttpServlet {
 	private static final long serialVersionUID = -7058685883212377590L;
@@ -32,13 +35,33 @@ public class Worker extends HttpServlet {
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} catch (ReservationException e) {
+			status.setStatus(QuotesStatus.Status.FAILED);
 		} finally {
 			em.close();
 			ois.close();
 		}
 	}
 	
-	private void confirmQuotes(List<Quote> quotes) { 
-		// TODO to implement
+	private void confirmQuotes(List<Quote> quotes) throws ReservationException { 
+		List<Reservation> reservations = new LinkedList<Reservation>();
+		EntityManager em = EMF.get().createEntityManager();
+		try {
+			for (Quote quote : quotes) {
+				reservations.add(confirmQuote(quote, em));
+			}
+		} catch(ReservationException exc) {
+			for (Reservation res : reservations) {
+				CarRentalCompany company = em.find(CarRentalCompany.class, res.getRentalCompany());
+				company.cancelReservation(res);
+			}
+			throw exc;
+		} finally {
+			em.close();
+		}
+	}
+	
+	private Reservation confirmQuote(Quote quote, EntityManager em) throws ReservationException {
+		return em.find(CarRentalCompany.class, quote.getRentalCompany()).confirmQuote(quote);
 	}
 }
